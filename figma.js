0037dc390b2d96a4315d6305033d3a0cf773c7f0
spider990 <85675202+spider990@users.noreply.github.com>
2022-04-12 20:21:51 +0200

    Add files via upload

---
+++ figma.js
@@ -0,0 +1,18 @@
+anime({
+    targets: '#text-svg path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 8000,
+    direction: 'alterante',
+    loop: true
+})
+
+
+anime({
+    targets: '.box',
+    width: '100%',
+    easing: 'easeInOutSine',
+    duration: 7000,
+    direction: 'alternate',
+    loop: true,
+})
